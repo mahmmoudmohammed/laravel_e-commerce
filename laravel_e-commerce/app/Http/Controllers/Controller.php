492942c3e53f7f060e80e9bf1822c6a8b25de766
Mahmoud Mohamed <mahmmoudmohamed2020@gmail.com>
2021-08-30 01:50:50 +0200

    insattl  swagger documentation

--- laravel_e-commerce/app/Http/Controllers/Controller.php
+++ laravel_e-commerce/app/Http/Controllers/Controller.php
@@ -7,6 +7,39 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="E-commerce Documentation",
+ *      description="L5 Swagger Api Documentation for Laravel E-commerce System ",
+ *      @OA\Contact(
+ *          email="mahmmoudmohammed2020@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Local API Server"
+ * )
+
+ *
+ * @OA\Tag(
+ *     name="E-commerce",
+ *     description="API Endpoints of E-commerce System"
+ * )
+ */
+
+/**
+ * @OA\Get(
+ *     path="/",
+ *     description="Home page",
+ *     @OA\Response(response="default", description="Welcome page")
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
