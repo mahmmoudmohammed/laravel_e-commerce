e3e433fd8ffd1c469990031d22f6263e55792055
Mahmoud Mohamed <mahmmoudmohamed2020@gmail.com>
2021-08-30 03:12:12 +0200

    add migrationn and spatie permission package

---
+++ laravel_e-commerce/database/migrations/2021_08_30_001639_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('in_stock');
+            $table->double('price');
+            $table->double('price_after');
+            $table->boolean('has_offer')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
